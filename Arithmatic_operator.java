e43fe8689a958a6d01387b8e6fb5826e9d0a0d5a
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-08-29 02:30:57 +0530

    Add files via upload

---
+++ Arithmatic_operator.java
@@ -0,0 +1,12 @@
+public class Arithmatic_operator{
+    public static void main(String args[]){
+        int a=20;
+        int b=5;
+        // Arithmetic Operators
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
---
+++ logical_operator.java
@@ -0,0 +1,8 @@
+public class logical_operator {
+    public static void main(String args[]){
+      System.out.println(3>1 && 5<2) ; // logical AND alltrue then true otherwise false
+      System.out.println(3>1 || 5<2) ; // logical OR any one true then true otherwise false
+      System.out.println(!(3>12)) ; // logical NOT reverse the result
+
+    }
+}
