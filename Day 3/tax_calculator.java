e736167d7cfdb77a8d4e63b525cb9eedcb40f257
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-08-30 01:09:16 +0530

    Add files via upload

---
+++ Day 3/tax_calculator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class tax_calculator {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter your income");
+    int income= sc.nextInt();
+    int tax;
+
+    if(income<500000){
+     tax =0;}
+
+    else if (income>=500000 && income<=1000000 ) {
+        tax = (int)(income*.20);}
+
+    else{
+        tax= (int)(income*.30);
+    }
+    System.out.println("Your tax is =" + tax);
+    sc.close();
+    }
+}
