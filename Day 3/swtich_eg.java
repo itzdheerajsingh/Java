023d7db8a2a6a0533748ae67f351765f7501ca38
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-08-30 02:30:45 +0530

    Add files via upload

---
+++ Day 3/swtich_eg.java
@@ -0,0 +1,19 @@
+public class swtich_eg {
+    public static void main(String args[]){
+        char ch = 'z';
+        switch (ch) {
+            case 'a': System.out.println("maggie");
+            break;
+            case 'b': System.out.println("coke");
+             break;
+            case 'c': System.out.println("biscuit");
+             break;
+            case 'd': System.out.println("munchies");
+             break;
+            default : System.out.println("we wake up");
+        }
+
+    }
+}
+
+// swich can be used for any datatype it can be float , int , character or other
