3732558b65dce17368e943975e2a33201c2682cc
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-08-31 23:00:42 +0530

    Add files via upload

---
+++ Day 4/brekeg1.java
@@ -0,0 +1,11 @@
+public class brekeg1 {
+   public static void main(String args[]){
+    for(int i=1;i<=10;i++){
+        if (i==3){
+            break;
+        }
+        System.out.println(i);
+    }
+    System.out.println("now it's Out of the loop");
+   } 
+}
---
+++ Day 4/dowhileeg.java
@@ -0,0 +1,9 @@
+public class dowhileeg {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Hello, World!");
+            count++;
+        } while (count <= 5);
+    }
+}
