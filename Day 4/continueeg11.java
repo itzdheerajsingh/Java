d977095aa1dd3c8fc33c6a27389512ff94b50c50
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-08-31 23:41:38 +0530

    Add files via upload

---
+++ Day 4/continueeg11.java
@@ -0,0 +1,11 @@
+public class continueeg11 {
+ public static void main (String args[])  {
+    for(int i=1;i<=10;i++){
+        if (i==3 || i==5){
+            continue;
+        }
+        System.out.println(i);
+    }
+    System.out.println("now it's Out of the loop");
+ } 
+}
