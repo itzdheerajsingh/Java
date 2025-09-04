29130842d6184069c3ed6f5eac8793238a6bb131
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-09-04 23:49:38 +0530

    Add files via upload

---
+++ Day 6/function.java
@@ -0,0 +1,15 @@
+public class function{
+    // statemnet modifier | return type | function name | parameters
+    // void means no return type
+    public static void printhello(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+         System.out.println("Hello World");
+          System.out.println("Hello World");
+        System.out.println("Hello World");
+        return ;
+    
+    }    public static void main(String args[]){
+        printhello(); // function call
+    }
+}
