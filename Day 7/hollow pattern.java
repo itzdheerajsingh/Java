17d5b4c222a28bdb60a41960647ad5d2764d14d5
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-09-07 17:46:04 +0530

    Update hollow pattern.java

--- Day 7/hollow pattern.java
+++ Day 7/hollow pattern.java
@@ -1,1 +1,24 @@
-
+//(5,4) hollow pryamid
+public class pryamid{
+    public static void pattern(int rows , int columns){
+       //outer loop for rows
+        for(int i=1;i<=rows;i++){
+            //inner loop for colomns
+            for(int j=1;j<=columns;j++ ){
+                //print star
+                if(i==1||i==4||j==1||j==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");}
+            }
+            System.out.println();
+            
+        }
+    }
+    public static void main(String args[]){
+        pattern(4,5);   
+        System.out.println();
+        
+    }
+}
