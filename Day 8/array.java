0bf06070afa2fe276d3c58a27d5777c4c88e24b0
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-09-08 04:43:44 +0530

    Update array.java

--- Day 8/array.java
+++ Day 8/array.java
@@ -1,1 +1,19 @@
+public class array{
+    public static void main(String args[]){
+        int arr[]= {15,20,35,40};
+//to print elements of array by index
+        System.out.println(arr[0]);// it will print element at index 0
+        System.out.println(arr[1]);// it will print element at index 1
+        System.out.println(arr[2]);// it will print element at index 2
+        System.out.println(arr[3]);// it will print element at index 3
+
+//to print elements of array by loop
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+
+
+    }
+    
+}
+}
 
