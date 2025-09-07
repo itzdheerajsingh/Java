d75889fec14cfac6f16a5d5dc1d616dc128cec53
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-09-08 03:25:52 +0530

    Add files via upload

---
+++ Day 7/diamond.java
@@ -0,0 +1,34 @@
+public class diamond {
+    public static void pattern(int n){
+        //upper loop for rows
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+    }
+    System.out.println();
+}
+
+{
+        //lower half loop for rows
+        for(int i=n;i>=1;i--){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+    }
+    System.out.println();
+}
+}
+}
+public static void main(String args[]){
+    pattern(5);
+}
+}
