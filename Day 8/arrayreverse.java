0a30555e966969dfa55c05aba3e318e3ccafc9b6
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-09-09 23:28:32 +0530

    Add files via upload

---
+++ Day 8/arrayreverse.java
@@ -0,0 +1,24 @@
+public class arrayreverse {
+    public static void reverse(int arr[]){
+        int start=0,end=arr.length-1;
+        while(start<end){
+            //swap
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        reverse(arr);
+        printarr(arr);
+    }
+}
