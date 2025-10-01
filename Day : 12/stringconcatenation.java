05e78929b0794f057bc5b2fe5a94bb3273c691df
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-10-01 23:37:04 +0530

    Add files via upload

---
+++ Day : 12/stringconcatenation.java
@@ -0,0 +1,9 @@
+public class stringconcatenation {
+    public static void main(String args[]){
+        String firstname="dheeraj";
+        String lastname="singh";
+        //concatenation
+        String fulllname=firstname+" "+lastname;
+        System.out.println("string is "+ fulllname);
+    }
+}
