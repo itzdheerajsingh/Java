263bd43ab4039142b566009cf0c1717ef348503a
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-10-04 15:19:00 +0530

    Add files via upload

---
+++ Day : 12/inbuiltsubstring.java
@@ -0,0 +1,7 @@
+public class inbuiltsubstring {
+    public static void main(String args[]){
+        String str="Engineer";
+        System.out.println(str.substring(0,4)); 
+        // si included, ei excluded
+    }
+}
