beee37f4af1fbe2de64ab28c764b8d121bd8ce9c
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-10-24 21:56:23 +0530

    Delete Checkpoweroftwo.java

--- Checkpoweroftwo.java
+++
@@ -1,9 +0,0 @@
-public class Checkpoweroftwo {
-    public static boolean check(int n){
-        //return true if n is a power of two
-       return (n& (n-1))==0;
-    }
-    public static void main(String args[]){
-        System.out.println(check(16));
-    }
-}
