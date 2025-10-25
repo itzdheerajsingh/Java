56d252ee7fb96e898d09d06378cda387951adee2
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-10-25 21:41:01 +0530

    Add files via upload

---
+++ Day : 14/constructor.java
@@ -0,0 +1,15 @@
+public class constructor {
+ public static void main(String[] args) {
+    student s1=new student("Dheeraj");
+    System.out.println(s1.name);
+ }   
+}
+class student{
+    String name;
+    int age;
+    student(String name){
+        this.name=name;
+        System.out.println("Constructor called...");
+        
+    }
+}
