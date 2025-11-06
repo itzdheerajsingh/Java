20bfd8e1458c0d4e56d2959c4dee81ef3b244ece
Dheeraj Singh <itzdheerajsingh@gmail.com>
2025-11-06 23:32:56 +0530

    Create SimpleArrayList with basic ArrayList operations

---
+++ Day : 19/SimpleArrayList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SimpleArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add elements
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        // Print elements
+        System.out.println("ArrayList: " + numbers);
+    }
+}
